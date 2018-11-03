58a440330b9a37a79cc3a75a1fca57a597641cf1
Juan Diego González <gorygon@Juans-MacBook-Pro.local>
2018-11-03 10:28:24 -0500

    cambios en la vista NOSQL

--- measurementsNOSQL/views.py
+++ measurementsNOSQL/views.py
@@ -7,6 +7,8 @@ from rest_framework.decorators import api_view, renderer_classes
 from rest_framework.parsers import JSONParser
 from rest_framework.generics import GenericAPIView
 from django.conf import settings
+from bson import Binary
+from bson.json_util import dumps
 import time
 
 
@@ -19,32 +21,44 @@ def measurementList(request):
         client = MongoClient(settings.DB_HOST, int(settings.DB_PORT))
         db = client[settings.MONGO_DB]
         db.authenticate( settings.MLAB_USER, settings.MLAB_PASSWORD)
-        measurements = db["measurements"]
+        measurements = db["variableSinPatrones"]
         data = measurements.find({})
-        for dto in data:
-            json_data = {
+        # for dto in data:
+        #     json_data = {
+
+        #        # "_id": dto["_id"],                
+        #         "elId": dto["elId"],
+        #         "measurement" : dto["measurement"],
+        #         "value" : dto["value"],
+        #         "unit" : dto["unit"],
+        #         "place" : dto["place"],
+        #         "time" : dto["time"]
                 
-                "id": dto["id"],
-                "measurement" : dto["measurement"],
-                "value" : dto["value"],
-                "unit" : dto["unit"],
-                "place" : dto["place"],
-                "time" : dto["time"]
-                
-            }
-            result.append(json_data)
+        #     }
+        #     result.append(dumps(data))
         client.close()
-        return JsonResponse(result, safe=False)
+        return JsonResponse(dumps(data).replace("\'", '"'), safe=False)
+    
     elif request.method == "POST":
         client = MongoClient(settings.DB_HOST, int(settings.DB_PORT))
         db = client[settings.MONGO_DB]
         db.authenticate( settings.MLAB_USER, settings.MLAB_PASSWORD)
         db = client[settings.MONGO_DB]
-        measurements = db["measurements"]
+        measurements = db["variableSinPatrones"]
         data = JSONParser().parse(request)
+        
+        
         idData = int((time.time()*1000) % 86400000)
-        data["id"] = idData
-        result = measurements.insert(data)
+        data["elId"] = idData
+        nombreAtributo = "measurement_"+data["place"]
+        # datoAgregar = {
+        #     nombreAtributo : data["value"]
+        # }
+        # result = measurements.findOneAndUpdate(
+        #     {"variable": data["measurement"]},
+        #     {"$set": {nombreAtributo: data["value"]}}
+        # )
+        result = measurements.find({})
         respo = {
             "MongoObjectID": str(result),
             "Message": "nuevo objeto en la base de datos"
@@ -64,13 +78,16 @@ def measurementDetail(request, pk, format=None):
         db = client[settings.MONGO_DB]
         measurements = db["measurements"]
         data = measurements.find({"id": pk})
+        print(data)
         for dto in data:
             jsonData = {
+               # "_id": dto["_id"],
                 "measurement" : dto["measurement"],
                 "value" : dto["value"],
                 "unit" : dto["unit"],
                 "place" : dto["place"],
-                "time" : dto["time"]
+                "time" : dto["time"],
+                "elId": dto["elId"]
             }
             result.append(jsonData)
         client.close()
